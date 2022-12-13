92b6fb4c1c30f5c2da933c48641e5f190a1a1808
Grey-1011 <z1251543107@gmail.com>
2022-12-13 20:46:48 +0800

    重构： 提取 WelcomeLayout 组件

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -2,21 +2,23 @@ import { defineComponent } from 'vue';
 import s from './First.module.scss';
 import icon from '../../assets/icons/balance.svg';
 import { RouterLink } from 'vue-router';
+import { WelcomeLayout } from './WelcomeLayout';
 
 export const First = defineComponent({
  setup(props, context) {
+  const slots = {
+    icon: () => <img class={s.icon} src={icon}/>,
+    title: () => <h2>会挣钱<br />还要会省钱</h2>,
+    buttons: () => <>
+      <RouterLink class={s.fake} to='/welcome/start'>跳过</RouterLink>
+      <RouterLink class={s.next} to="/welcome/2">下一页</RouterLink>
+      <RouterLink to='/welcome/start'>跳过</RouterLink>
+    </>
+  }
   return () => (
-    <div class={s.wrapper}>
-      <div class={s.card}>
-        <img class={s.icon} src={icon}/>
-        <h2>会挣钱<br />还要会省钱</h2>
-      </div>
-      <div class={s.actions}>
-        <RouterLink class={s.fake} to='/welcome/start'>跳过</RouterLink>
-        <RouterLink class={s.next} to="/welcome/2">下一页</RouterLink>
-        <RouterLink to='/welcome/start'>跳过</RouterLink>
-      </div>
-    </div>
+    <WelcomeLayout v-slots={slots}>
+ 
+    </WelcomeLayout>
   )
  }
 })
