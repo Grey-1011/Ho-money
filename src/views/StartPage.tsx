c532a332a6c505f8b5ea707f45add258fe3301af
Grey-1011 <z1251543107@gmail.com>
2023-01-03 21:18:15 +0800

    创建 MainLayout 组件

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,10 +1,10 @@
 import { defineComponent, ref } from 'vue';
 import { RouterLink } from 'vue-router';
+import { MainLayout } from '../layouts/MainLayout';
 import { Button } from '../shared/Button';
 import { Center } from '../shared/Center';
 import { FloatButton } from '../shared/FloatButton';
 import { Icon } from '../shared/Icon';
-import { Navbar } from '../shared/Navbar';
 import { Overlay } from '../shared/Overlay';
 import s from './StartPage.module.scss';
 
@@ -16,28 +16,26 @@ export const StartPage = defineComponent({
   }
 
   return () => (
-    <div>
-      <div>
-        <Navbar>
-          {
-            {default: () => '不叮记账', 
-            icon: () => <Icon name='menu' class={s.navIcon} onClick={onClickMenu}/> }
-          }
-        </Navbar>
-        { overlayVisible.value && <Overlay onClose={() => overlayVisible.value = false}/> }
-      </div>
-      <Center class={s.wallet_wrapper}>
-        <Icon name='wallet' class={s.wallet}/>
-      </Center>
-      <div class={s.button_wrapper}>
-        <RouterLink to="/items/create">
-          <Button class={s.button}>开始记账</Button>
-        </RouterLink>
-      </div>
-      <RouterLink to="/items/create">
-        <FloatButton iconName='add'/>
-      </RouterLink>
-    </div>
+    <MainLayout>{
+      {
+        title: () => '不叮记账',
+        icon: () => <Icon name='menu' class={s.navIcon} onClick={onClickMenu}/>,
+        default: () => <>
+          <Center class={s.wallet_wrapper}>
+            <Icon name='wallet' class={s.wallet}/>
+          </Center>
+          <div class={s.button_wrapper}>
+            <RouterLink to="/items/create">
+              <Button class={s.button}>开始记账</Button>
+            </RouterLink>
+          </div>
+          <RouterLink to="/items/create">
+            <FloatButton iconName='add'/>
+          </RouterLink>
+          { overlayVisible.value && <Overlay onClose={() => overlayVisible.value = false}/> }
+        </>
+      }
+    }</MainLayout>
   )
  }
 })
