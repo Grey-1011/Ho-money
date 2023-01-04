c7b4ec0289a3c91de9b86f1472503ee7af662353
Grey-1011 <z1251543107@gmail.com>
2023-01-04 21:49:27 +0800

    封装 time.tsx

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -1,5 +1,6 @@
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, ref } from 'vue';
 import { Icon } from '../../shared/Icon';
+import { time } from '../../shared/time';
 import s from './InputPad.module.scss';
 export const InputPad = defineComponent({
   props: {
@@ -8,6 +9,9 @@ export const InputPad = defineComponent({
     }
   },
   setup(props, context) {
+    const data = ref<Date>();
+    const now = new Date();
+
     const buttons = [
       {text: '1', onClick: () => {}},
       {text: '2', onClick: () => {}},
@@ -30,7 +34,9 @@ export const InputPad = defineComponent({
       <div class={s.dateAndAmount}>
         <span class={s.date}>
           <Icon name='date' class={s.icon}/>
-          <span>2023-01-01</span>
+          <span>
+            <input type='date' value={time(now).format()}></input>
+          </span>
         </span>
         <span class={s.amount}>123456789</span>
       </div>
