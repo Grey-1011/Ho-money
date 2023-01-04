d9f37cd9b93c412b0d009b4f02ce4a39b5d26a00
Grey-1011 <z1251543107@gmail.com>
2023-01-04 13:53:05 +0800

    修复 欢迎界面 跳过路由 路径的错误

--- src/components/welcome/FirstAction.tsx
+++ src/components/welcome/FirstAction.tsx
@@ -3,9 +3,9 @@ import { RouterLink } from 'vue-router';
 
 export const FirstAction = () => (
   <div class={s.actions}>
-    <RouterLink class={s.fake} to='/welcome/start'>跳过</RouterLink>
+    <RouterLink class={s.fake} to='/start'>跳过</RouterLink>
     <RouterLink class={s.next} to="/welcome/2">下一页</RouterLink>
-    <RouterLink to='/welcome/start'>跳过</RouterLink>
+    <RouterLink to='/start'>跳过</RouterLink>
   </div>
 )
 
--- src/components/welcome/ThirdAction.tsx
+++ src/components/welcome/ThirdAction.tsx
@@ -3,9 +3,9 @@ import { RouterLink } from 'vue-router';
 
 export const ThirdAction = () => (
   <div class={s.actions}>
-    <RouterLink class={s.fake} to='/welcome/start'>跳过</RouterLink>
+    <RouterLink class={s.fake} to='/start'>跳过</RouterLink>
     <RouterLink class={s.next} to="/welcome/4">下一页</RouterLink>
-    <RouterLink to='/welcome/start'>跳过</RouterLink>
+    <RouterLink to='/start'>跳过</RouterLink>
   </div>
 )
 
