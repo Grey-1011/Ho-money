8132fda09bae4d4796b100c00301f137005624de
Grey-1011 <z1251543107@gmail.com>
2023-01-04 09:56:29 +0800

    wip: Tabs 组件

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -1,6 +1,7 @@
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, ref } from 'vue';
 import { MainLayout } from '../../layouts/MainLayout';
 import { Icon } from '../../shared/Icon';
+import { Tab, Tabs } from '../../shared/Tabs';
 import s from './ItemCreate.module.scss';
 export const ItemCreate = defineComponent({
   props: {
@@ -9,14 +10,25 @@ export const ItemCreate = defineComponent({
     }
   },
   setup(props, context) {
+    const kind = ref('支出');
+    const onUpdateSelected = (name: string) => {
+      kind.value = name
+    }
     return () => (
       <MainLayout>{
         {
           title: () => '记一笔',
           icon: () => <Icon name='back' class={s.navIcon}/>,
-          default: () => (
-            <div>main</div>
-          )
+          default: () => <>
+          <Tabs v-model:selected={kind.value}>
+            <Tab name="支出">
+              icon 列表
+            </Tab>
+            <Tab name="收入">
+              icon 列表2
+            </Tab>
+          </Tabs>
+        </>
         }  
       }</MainLayout>
     )
