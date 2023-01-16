32970198b4c8021c076bf717c76638384ca409b8
Grey-1011 <z1251543107@gmail.com>
2023-01-16 16:37:48 +0800

    wip: 创建 TagEdit.tsx

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -1,17 +1,13 @@
-import { defineComponent, PropType, reactive, toRaw } from 'vue';
+import { defineComponent, reactive } from 'vue';
 import { MainLayout } from '../../layouts/MainLayout';
 import { Button } from '../../shared/Button';
 import { EmojiSelect } from '../../shared/EmojiSelect';
 import { Icon } from '../../shared/Icon';
-import s from './TagCreate.module.scss';
+import s from './Tag.module.scss';
 import { Rules, validate } from '../../shared/validate';
 
 export const TagCreate = defineComponent({
-  props: {
-    name: {
-      type: String as PropType<string>,
-    }
-  },
+
   setup(props, context) {
     const formData = reactive({
       name: '',
@@ -19,7 +15,6 @@ export const TagCreate = defineComponent({
     })
     const errors = reactive<{ [k in keyof typeof formData]?: string[] }>({})
     const onSubmit = (e: Event) => {
-      console.log(toRaw(formData));
       const rules: Rules<typeof formData> = [
         { key: 'name', type: 'required', message: '必填' },
         { key: 'name', type: 'pattern', regex: /^.{1,4}$/, message: '只能填 1~4 个字符' },
