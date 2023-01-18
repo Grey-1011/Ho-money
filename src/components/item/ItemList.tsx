9bccda8da3537bfc539ca18eb1ae07ce11a2ddab
Grey-1011 <z1251543107@gmail.com>
2023-01-18 12:33:28 +0800

    引入 Vant <Overlay> 组件，打算封装 <Form>组件

--- src/components/item/ItemList.tsx
+++ src/components/item/ItemList.tsx
@@ -1,4 +1,5 @@
-import { defineComponent, PropType, reactive, ref } from 'vue';
+import { Overlay } from 'vant';
+import { defineComponent, PropType, reactive, ref, watchEffect } from 'vue';
 import { MainLayout } from '../../layouts/MainLayout';
 import { Icon } from '../../shared/Icon';
 import { Tab, Tabs } from '../../shared/Tabs';
@@ -19,13 +20,19 @@ export const ItemList = defineComponent({
       { start: time.add(-1, 'month').firstDayOfMonth(), end: time.add(-1, 'month').lastDayOfMonth() },
       { start: time.firstDayOfYear(), end: time.lastDayOfYear() }
     ]
-   
+    
+    watchEffect(() => {
+      if (refSelected.value === '自定义时间') {
+        refOverlayVisible.value = true
+      }
+    })
+    const refOverlayVisible = ref(true)
     return () => (
       <MainLayout>{
        {
         titie: () => '不叮记账',
         icon: () => <Icon name="menu"/>,
-        default: () => (
+        default: () => <>
           <Tabs classPrefix={'customTabs'} v-model:selected={refSelected.value}>
             <Tab name='本月'>
               <ItemSummary startDate={ timeList[0].start.format() } endDate={ timeList[0].end.format() }/>
@@ -40,7 +47,25 @@ export const ItemList = defineComponent({
             <ItemSummary startDate={ customTime.start.format() } endDate={ customTime.end.format() }/>
             </Tab>
           </Tabs>
-        )
+
+          <Overlay show={refOverlayVisible.value} class={s.overlay}>
+          <div class={s.overlay_inner}>
+            <header>
+              请选择时间
+            </header>
+            <main>
+              <form>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+              </form>
+            </main>
+          </div>
+          </Overlay>
+        </>
        } 
       }</MainLayout>
     )
