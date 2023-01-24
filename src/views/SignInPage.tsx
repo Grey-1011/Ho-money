514255b951446004ad8190031cccb3c55edf2c90
Grey-1011 <z1251543107@gmail.com>
2023-01-24 12:21:19 +0800

    测试 发送验证码的 接口

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { defineComponent, PropType, reactive } from 'vue';
 import { MainLayout } from '../layouts/MainLayout';
 import { Button } from '../shared/Button';
@@ -30,9 +31,9 @@ export const SignInPage = defineComponent({
 
       Object.assign(errors, newErrors)
     }
-    const onClickSendValidationCode = () => {
-      console.log('1111');
-      
+    const onClickSendValidationCode = async () => {
+      const response = await axios.post('/api/v1/validation_codes', { email: formData.email})
+      console.log(response);
     }
 
     return () => (
--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,12 @@ export default defineConfig({
       mergeProps: true
     }),
     svgstore(),
-  ]
+  ],
+  server:{
+    proxy:{
+      '/api/v1': {
+        target: 'http://121.196.236.94:8080'
+      }
+    }
+  }
 })
