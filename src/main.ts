70b25605469cc25bc5c79eb8b94f0e206da312bc
Grey-1011 <z1251543107@gmail.com>
2023-01-26 18:00:09 +0800

    优化全局路由守卫

--- src/main.ts
+++ src/main.ts
@@ -5,25 +5,32 @@ import { routes } from './config/routes';
 import { history } from './shared/history';
 import "@svgstore";
 import { http } from './shared/Http';
-import { mePromise, refreshMe } from './shared/me';
+import { fetchMe, mePromise } from './shared/me';
 
 const router = createRouter({
   history,
   routes
 })
 
-refreshMe()
+const whiteList: Record<string, 'exact' | 'startsWith'> = {
+  '/': 'exact',
+  '/start': 'exact',
+  '/welcome': 'startsWith',
+  '/sign_in': 'startsWith'
+}
+
+fetchMe()
 
 router.beforeEach(async (to, from) => {
-  if(to.path === '/' || to.path.startsWith('/welcome') || to.path.startsWith('/sign_in') || to.path === '/start'){
-    return true
-  }else {
-    const path =  await mePromise!.then(
-      () => true,
-      () => { return '/sign_in?return_to=' + to.path }
-    )
-    return path
+  for(const key in whiteList){
+    const value = whiteList[key]
+    if(value === 'exact' && to.path === key) return true
+    if(value === 'startsWith' && to.path.startsWith(key)) return true
   }
+  return mePromise!.then(
+    () => true,
+    () => '/sign_in?return_to=' + to.path
+  )
 })
 
 const app = createApp(App)
