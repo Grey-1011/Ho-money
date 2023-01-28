828c351bd2d7ff5ea78cca453f043900c0266084
Grey-1011 <z1251543107@gmail.com>
2023-01-28 22:44:26 +0800

    封装 Tags 组件

---
+++ src/components/item/Tags.tsx
@@ -0,0 +1,50 @@
+import { Icon, Button } from 'vant';
+import { defineComponent, PropType } from 'vue';
+import { http } from '../../shared/Http';
+import { useTags } from '../../shared/useTags';
+import s from './Tags.module.scss';
+export const Tags = defineComponent({
+  props: {
+    kind: {
+      type: String as PropType<string>,
+      required: true
+    }
+  },
+  setup(props, context) {
+    const { tags, hasMore, fetchTags: fetchTags } = useTags((page) => {
+      return http.get<Resources<Tag>>('/tags', {
+        kind: props.kind,
+        page: page + 1,
+        _mock: 'tagIndex',
+      })
+    })
+
+    return () => (
+      <>
+        <div class={s.tags_wrapper}>
+          <div class={s.tag}>
+            <div class={s.sign}>
+              <Icon name="add" class={s.createTag} />
+            </div>
+            <div class={s.name}>
+              新增
+            </div>
+          </div>
+            {tags.value.map(tag =>
+              <div class={[s.tag, s.selected]}>
+                <div class={s.sign}>
+                  {tag.sign}
+                </div>
+                <div class={s.name}>
+                  {tag.name}
+                </div>
+              </div>)
+            }
+        </div>
+        <div class={s.more}>
+          { hasMore.value ? <Button onClick={fetchTags} class={s.loadMore}>加载更多</Button> : <span class={s.noMore}>没有更多数据了</span> }
+        </div>
+      </>
+    )
+  }
+})
