805049acfe9c0878f6cb6ac00cc78d91119d4466
Grey-1011 <z1251543107@gmail.com>
2023-01-29 17:50:53 +0800

    格式化文档

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -10,62 +10,71 @@ import { InputPad } from './InputPad';
 import s from './ItemCreate.module.scss';
 import { Tags } from './Tags';
 export const ItemCreate = defineComponent({
-
   setup(props, context) {
     const formData = reactive({
       kind: '支出',
       tags_id: [],
       happen_at: new Date().toISOString(),
       amount: 0,
-    })
+    });
 
-    
     const onError = (error: AxiosError<ResourceError>) => {
       if (error.response?.status === 422) {
         showDialog({
           title: '出错',
-          message: Object.values(error.response.data.errors).join('\n')
-        })
+          message: Object.values(error.response.data.errors).join('\n'),
+        });
       }
-      throw error
-    }
-    const router = useRouter()
+      throw error;
+    };
+    const router = useRouter();
 
-    const onSubmit = async() => {
-      await http.post<Resource<Item>>('/items', formData, {
-        params: { _mock: 'itemCreate' }
-      }).catch(onError)
+    const onSubmit = async () => {
+      await http
+        .post<Resource<Item>>('/items', formData,{
+          params: { _mock: 'itemCreate' },
+        })
+        .catch(onError);
 
-      router.push('/items')
-    }
-    
-  
+      router.push('/items');
+    };
+
     return () => (
-      <MainLayout class={s.layout}>{
-        {
+      <MainLayout class={s.layout}>
+        {{
           title: () => '记一笔',
-          icon: () => <Icon name='back' class={s.navIcon}/>,
-          default: () => <>
-         <div class={s.wrapper}>
-            <Tabs v-model:selected={formData.kind} class={s.tabs}>
-              <Tab name="支出">
-                <Tags kind='expenses' key='expenses' v-model:selected={formData.tags_id[0]}/>
-              </Tab>
-              <Tab name="收入" >
-                <Tags kind='income' key='income' v-model:selected={formData.tags_id[0]}/>
-              </Tab>
-            </Tabs>
-            <div class={s.inputPad_wrapper}>
-              <InputPad 
-                v-model:happenAt={formData.happen_at}
-                v-model:amount={formData.amount}
-                onSubmit={onSubmit}
-              />
-            </div>
-          </div>
-        </>
-        }  
-      }</MainLayout>
-    )
-  }
-})
+          icon: () => <Icon name="back" class={s.navIcon} />,
+          default: () => (
+            <>
+              <div class={s.wrapper}>
+                <Tabs v-model:selected={formData.kind} class={s.tabs}>
+                  <Tab name="支出">
+                    <Tags
+                      kind="expenses"
+                      key="expenses"
+                      v-model:selected={formData.tags_id[0]}
+                    />
+                  </Tab>
+                  <Tab name="收入">
+                    <Tags
+                      kind="income"
+                      key="income"
+                      v-model:selected={formData.tags_id[0]}
+                    />
+                  </Tab>
+                </Tabs>
+                <div class={s.inputPad_wrapper}>
+                  <InputPad
+                    v-model:happenAt={formData.happen_at}
+                    v-model:amount={formData.amount}
+                    onSubmit={onSubmit}
+                  />
+                </div>
+              </div>
+            </>
+          ),
+        }}
+      </MainLayout>
+    );
+  },
+});
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,46 +1,50 @@
 /// <reference types="vite/client" />
 
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
+  import type { DefineComponent } from 'vue';
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-  const component: DefineComponent<{}, {}, any>
-  export default component
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
 }
 
-type JSONValue = string | number | null | boolean | JSONValue[] | { [key: string]: JSONValue }
+type JSONValue =
+  | string
+  | number
+  | null
+  | boolean
+  | JSONValue[]
+  | { [key: string]: JSONValue };
 
 type Tag = {
-  id: number,
-  user_id: number,
-  name: string,
-  sign: string,
-  kind: expenses | income
-}
+  id: number;
+  user_id: number;
+  name: string;
+  sign: string;
+  kind: expenses | income;
+};
 
 type Resources<T = any> = {
-  resources: T[],
+  resources: T[];
   pager: {
-    page: number,
-    per_page: number,
-    count: number
-  }
-}
-
+    page: number;
+    per_page: number;
+    count: number;
+  };
+};
 
 type Item = {
-  id: number
-  user_id: number
-  amount: number
-  tags_id: number[]
-  happen_at: string
-  kind: expenses | income
-}
-
+  id: number;
+  user_id: number;
+  amount: number;
+  tags_id: number[];
+  happen_at: string;
+  kind: expenses | income;
+};
 
 type Resource<T> = {
-  resource: T
-}
+  resource: T;
+};
 
 type ResourceError = {
-  errors: Record<string, string[]>
-}
+  errors: Record<string, string[]>;
+};
