9b6f355573482257a53036b4d91f2b53902bd2d7
Grey-1011 <z1251543107@gmail.com>
2023-01-30 11:38:03 +0800

    触发长按效果

--- src/components/item/Tags.tsx
+++ src/components/item/Tags.tsx
@@ -1,5 +1,6 @@
 import { Icon, Button } from 'vant';
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, ref } from 'vue';
+import { RouterLink } from 'vue-router';
 import { http } from '../../shared/Http';
 import { useTags } from '../../shared/useTags';
 import s from './Tags.module.scss';
@@ -24,21 +25,47 @@ export const Tags = defineComponent({
     const onSelect = (tag: Tag) => {
       context.emit('update:selected', tag.id)
     }
+
+    const timer = ref<number>()
+    const currentTag = ref<HTMLDivElement>()
+
+    const onLongPress = () => {
+      console.log('长按');
+    }
+
+    const onTouchStart = (e: TouchEvent) => {
+      currentTag.value = e.target as HTMLDivElement
+      timer.value = setTimeout(()=>{
+        onLongPress()
+      }, 500)
+    }
+
+    const onTouchMove = (e: TouchEvent) => {
+      const pointedElement = document.elementFromPoint(e.touches[0].clientX, e.touches[0].clientY)
+      if(currentTag.value?.contains(pointedElement) === false && pointedElement !== currentTag.value){
+        clearTimeout(timer.value)
+      }
+    }
+
+    const onTouchEnd = (e: TouchEvent) => {
+      clearTimeout(timer.value)
+    }
+
     return () => (
       <>
-        <div class={s.tags_wrapper}>
-          <div class={s.tag}>
-            <div class={s.sign}>
-              <Icon name="add" class={s.createTag} />
-            </div>
-            <div class={s.name}>
-              新增
-            </div>
-          </div>
+        <div class={s.tags_wrapper} onTouchmove={onTouchMove}>
+            <RouterLink to={`/tags/create?kind=${props.kind}`} class={s.tag}>
+              <div class={s.sign}>
+                <Icon name="add" class={s.createTag} />
+              </div>
+              <div class={s.name}>新增</div>
+            </RouterLink>
             {tags.value.map(tag =>
               <div 
                 class={[s.tag, props.selected === tag.id ? s.selected : ' ']}
                 onClick={() => {onSelect(tag)}}
+                onTouchstart={onTouchStart}
+                onTouchend={onTouchEnd}
               >
                 <div class={s.sign}>
                   {tag.sign}
