1273bf23cea96926b34a278b72f0990e00b3ce43
Grey-1011 <z1251543107@gmail.com>
2023-01-30 22:18:55 +0800

    封装 Money

---
+++ src/shared/Money.tsx
@@ -0,0 +1,24 @@
+import { defineComponent, PropType } from "vue";
+
+export const Money = defineComponent({
+  props: {
+    value: {
+      type: Number as PropType<number>,
+      required: true
+    }
+  }, 
+  setup: (props,context) => {
+    const addZero = (n: number) => {
+      const nString = n.toString()
+      const dotIndex = nString.indexOf('.')
+      if(dotIndex < 1){
+        return n + '.00'
+      }else if(nString.substring(dotIndex).length == 2){
+        return n + '0'
+      }else {
+        return n
+      }
+    }
+    return () => <span>{addZero(props.value / 100)}</span>
+  }
+})
